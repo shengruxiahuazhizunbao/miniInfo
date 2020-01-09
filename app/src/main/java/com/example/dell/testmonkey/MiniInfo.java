1b300e8ac1149bed12d21b57c5b0de663be63341
shengruxiahuazhizunbao <604849466@qq.com>
2020-01-09 09:44:01 +0800

    modify_1

--- app/src/main/java/com/example/dell/testmonkey/MiniInfo.java
+++ app/src/main/java/com/example/dell/testmonkey/MiniInfo.java
@@ -2,5 +2,5 @@ package com.example.dell.testmonkey;
 
 public class MiniInfo {
     public static final String PHONR="15068868287";
-    public static final String ADDRESS="黄石";
+    public static final String ADDRESS="内地";
 }
