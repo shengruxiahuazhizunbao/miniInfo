283911540cc969ec13098dcf63c3349aaa45c379
shengruxiahuazhizunbao <604849466@qq.com>
2020-01-09 10:39:27 +0800

    branch 修改

--- app/src/main/java/com/example/dell/testmonkey/MiniInfo.java
+++ app/src/main/java/com/example/dell/testmonkey/MiniInfo.java
@@ -3,4 +3,6 @@ package com.example.dell.testmonkey;
 public class MiniInfo {
     public static final String PHONR="15068868287";
     public static final String ADDRESS="内地";
+
+    public static final String NAME="王大锤";
 }
