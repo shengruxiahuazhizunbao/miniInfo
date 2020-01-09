7a5246209b0bd9686ff14a9ff8717f0ac8400cb4
shengruxiahuazhizunbao <604849466@qq.com>
2020-01-09 15:36:45 +0800

    修改master

--- app/src/main/java/com/example/dell/testmonkey/MaxName.java
+++ app/src/main/java/com/example/dell/testmonkey/MaxName.java
@@ -3,5 +3,5 @@ package com.example.dell.testmonkey;
 public class MaxName {
 
     public static final String FOCOUS="哇哈哈mm";
-    public static final String MP="DJDKYY";
+    public static final String MP="DJDKYYLP";
 }
